c657da7d63ad709f8cb336d0f0cdb7f44e40b0a5
christophe-essec <christophe.harrer@essec.edu>
2017-03-22 19:48:53 +0100

    With the satbase now

--- app.js
+++ app.js
@@ -1,38 +1,57 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs  = require('express-handlebars');
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
 var clientList = [];
 
-//Permet de lire les paramètres envoyés dans un formulaire
-app.use(bodyParser.urlencoded({extended:true}));
 
-//Permet d'utiliser Handlebars comme moteur de rendu de template HTML
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+var url = 'mongodb://localhost:27017/professorspace';
 
-//Permet d'utiliser le rep 'Static' pour envoyer des fichiers non dynamiques
-app.use(express.static("static"));
+// Use connect method to connect to the Server 
+MongoClient.connect(url, function(err, db) {
+  console.log("Connected correctly to server",err);
+ 
+  
 
-app.get("/hello",function(req,res){
-  res.send("Hello World");
-});
+	//Permet de lire les paramètres envoyés dans un formulaire
+	app.use(bodyParser.urlencoded({extended:true}));
 
-app.post("/newuser",function(req,res){
+	//Permet d'utiliser Handlebars comme moteur de rendu de template HTML
+	app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+	app.set('view engine', 'handlebars');
 
-	clientList.push(req.body.clientName);
-	console.log(clientList);
+	//Permet d'utiliser le rep 'Static' pour envoyer des fichiers non dynamiques
+	app.use(express.static("static"));
 
-	res.redirect("/");
-});
-
-app.get('/', function (req, res) {
-	var result = clientList.map(function(item,index){
-		return {name:item,number:index};
+	app.get("/hello",function(req,res){
+	  res.send("Hello World");
 	});
 
-    res.render('home',{clients:result});
+	app.post("/newuser",function(req,res){
+
+		db.collection('clients').insert({name : req.body.clientName}, function(err, result) {
+
+    		console.log("Insertion réalisée",err,result);
+    		//db.close();
+    		res.redirect("/");
+  		});
+
+	});
+
+	app.get('/', function (req, res) {
+		db.collection('clients').find({}).toArray(function(err,clients){
+			console.log("clients",clients);
+			var result = clients.map(function(item,index){
+				item.number = index;
+				return item;
+			});
+
+		    res.render('home',{clients:result});
+		});
+	});
+
 });
 
 app.listen(8080,function(err){
